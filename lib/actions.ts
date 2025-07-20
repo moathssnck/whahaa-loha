18f620ff288544df22f29aaed50a7f1c9e01adcf
moathssnck <myneww00@gmail.com>
2025-07-20 22:01:17 +0300

    ssss

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/ptt_radio.mp3')
+    const audio=new Audio('/gd.wav')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
